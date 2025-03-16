dbec67dfc32ae42ffc72708453d22ea42caee01e
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-03-17 00:56:56 +0600

    set the protectedRoute all dashboard

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -2,7 +2,7 @@
 import {configureStore} from '@reduxjs/toolkit'
 import authReducer from "./auth/authSlice"
 import { baseApi } from '../api/baseApi'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
  
  
@@ -21,7 +21,13 @@ export const store=configureStore({
                       
                         
             },
-            middleware:(getDefaultMiddleWares)=>getDefaultMiddleWares().concat(baseApi.middleware)
+            middleware:(getDefaultMiddleWares)=>getDefaultMiddleWares(
+                        {
+                                    serializableCheck: {
+                                      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+                                    },
+                                  }
+            ).concat(baseApi.middleware)
 })
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
