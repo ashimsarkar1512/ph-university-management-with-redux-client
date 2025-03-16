ad58eefed2c542039b257fd0cd20c1f715ffd631
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-03-16 13:44:44 +0600

    set the redux persist in project

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,51 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/features/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
+
 const Login=()=>{
+
+            const dispatch=useAppDispatch();
+
+            const{register,handleSubmit} =useForm();
+            const [login,{error}]=useLoginMutation();
+           
+
+            const onSubmit= async(data)=>{
+                        const userInfo={
+                                    id:data.userId,
+                                    password:data.password
+
+
+                        };
+                        
+
+                        const res=await login(userInfo).unwrap()
+
+                        const user=verifyToken(res.data.accessToken)
+
+                        dispatch(setUser({user:user,token:res.data.accessToken}))
+
+
+            }
+
+          
             return(
-                        <h1>this is Login content</h1>
+                       <form onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                                    <label htmlFor="id">Id:</label>
+                                    <input type="text" id="id" {...register('userId')}/>
+                        </div>
+                        <div>
+                                    <label htmlFor="password">Password:</label>
+                                    <input type="text" id="password" {...register('password')}/>
+                        </div>
+                        <Button htmlType="submit">Login</Button>
+
+                       </form>
             )
 }
 
--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,7 +1,27 @@
 
 import {configureStore} from '@reduxjs/toolkit'
+import authReducer from "./auth/authSlice"
+import { baseApi } from '../api/baseApi'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+ 
+ 
+const persistConfig = {
+  key: 'auth',
+  storage,
+}
+ 
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
+
+
 export const store=configureStore({
-            reducer:{},
+            reducer:{
+                        [baseApi.reducerPath]:baseApi.reducer,
+                        auth:persistedAuthReducer,
+                      
+                        
+            },
+            middleware:(getDefaultMiddleWares)=>getDefaultMiddleWares().concat(baseApi.middleware)
 })
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
@@ -9,3 +29,5 @@ export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 export type AppStore = typeof store
+
+export const persistor=persistStore(store)
