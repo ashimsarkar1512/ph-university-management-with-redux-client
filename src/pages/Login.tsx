5e6592437ba7a690980e3bdde4fe8bee23520397
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-03-21 21:44:04 +0600

    set up the login from and add the antd and react hook form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,64 +1,61 @@
-import { Button } from "antd";
-import { FieldValues, useForm } from "react-hook-form";
-import { useLoginMutation } from "../redux/features/auth/authApi";
-import { useAppDispatch } from "../redux/features/hooks";
-import { setUser, TUser } from "../redux/features/auth/authSlice";
-import { verifyToken } from "../utils/verifyToken";
-import { useNavigate } from "react-router-dom";
-import { toast } from "sonner";
+import { Button, Row } from 'antd';
+import { FieldValues } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
 
+import { TUser, setUser } from '../redux/features/auth/authSlice';
+import { verifyToken } from '../utils/verifyToken';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
-const Login=()=>{
-            const navigate=useNavigate()
+import PHInput from '../components/form/PHInput';
+import PHForm from '../components/form/PHFrom';
+import { useAppDispatch } from '../redux/features/hooks';
 
-            const dispatch=useAppDispatch();
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  // const { register, handleSubmit } = useForm({
+  //   defaultValues: {
+  //     userId: 'A-0002',
+  //     password: 'admin123',
+  //   },
+  // });
 
-            const{register,handleSubmit} =useForm();
-            const [login]=useLoginMutation();
-           
+  const defaultValues = {
+    userId: 'A-0002',
+    password: 'admin123',
+  };
 
-            const onSubmit= async(data :FieldValues)=>{
+  const [login] = useLoginMutation();
 
-                        const toastId=toast.loading('loggin in')
-                        try{
-                                    const userInfo={
-                                                id:data.userId,
-                                                password:data.password
-            
-            
-                                    };
-                                    
-            
-                                    const res=await login(userInfo).unwrap()
-            
-                                    const user=verifyToken(res.data.accessToken) as TUser
-            
-                                    dispatch(setUser({user:user,token:res.data.accessToken}))
-                                    toast.success('logged in',{id:toastId,duration:2000})
-            
-                                    navigate(`/${user.role}/dashboard`)
-                        }catch(err){
-                          toast.error('something went wrong',{id:toastId,duration:2000})
-                        }
+  const onSubmit = async (data: FieldValues) => {
+    console.log(data);
+    const toastId = toast.loading('Logging in');
 
+    try {
+      const userInfo = {
+        id: data.userId,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success('Logged in', { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (err) {
+      toast.error('Something went wrong', { id: toastId, duration: 2000 });
+    }
+  };
 
-            }
-
-          
-            return(
-                       <form onSubmit={handleSubmit(onSubmit)}>
-                        <div>
-                                    <label htmlFor="id">Id:</label>
-                                    <input type="text" id="id" {...register('userId')}/>
-                        </div>
-                        <div>
-                                    <label htmlFor="password">Password:</label>
-                                    <input type="text" id="password" {...register('password')}/>
-                        </div>
-                        <Button htmlType="submit">Login</Button>
-
-                       </form>
-            )
-}
+  return (
+    <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PHInput type="text" name="userId" label="ID:" />
+        <PHInput type="text" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
+  );
+};
 
 export default Login;
